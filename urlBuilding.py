aa4811541e734fcf6fc288255a6616ff47e7abe8
mattsanpedro7 <mattsanpedro76@gmail.com>
2018-06-21 16:18:56 -0700

    building url to a specific function

---
+++ urlBuilding.py
@@ -0,0 +1,29 @@
+# URL building
+# accepts name of the function as first argument
+# and any number of keyword arguments, each corresponding to variable part
+# unknown variables are appended to the URL as query parameters
+from flask import Flask, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'index'
+
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/user/<username>')
+def profile(username):
+    return '{}\'s profile'.format(username)
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
+    
+    
+    
+    
