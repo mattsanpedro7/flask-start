cc763865447e17c12188424b17677ef156c413f2
mattsanpedro7 <mattsanpedro76@gmail.com>
2018-06-22 01:02:25 -0700

    begin rendering templates doc

---
+++ quickstart/renderingTemplates.py
@@ -0,0 +1,9 @@
+# render a template
+
+from flask import render_template
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
